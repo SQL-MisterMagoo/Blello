a3074c254e9c67f6811a5dfc1db2ed3ccac8365b
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-03-24 16:58:03 +0000

    Drag and drop within a board working, Shift during drop means copy

---
+++ Blello/Model/BoardItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blello.Model
+{
+    public class BoardItem
+    {
+        public int BoardId { get; set; }
+        public string Content { get; set; }
+    }
+}
