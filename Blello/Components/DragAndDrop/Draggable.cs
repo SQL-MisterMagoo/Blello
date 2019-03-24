c125f61d144da92c0a4dd65523e03c6497034b00
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-03-24 16:06:53 +0000

    Add project files.

---
+++ Blello/Components/DragAndDrop/Draggable.cs
@@ -0,0 +1,87 @@
+﻿using Blello.Helpers;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Blello.Components.DragAndDrop
+{
+    public class Draggable<TItem> : ComponentBase
+    {
+        /// <summary>
+        /// html element id - defaults to pseudo random to prevent diff problems
+        /// </summary>
+        [Parameter] protected string ID { get; set; } = Guid.NewGuid().GetHashCode().ToString();
+        /// <summary>
+        /// place your markup here
+        /// </summary>
+        [Parameter] protected RenderFragment<TItem> DragContent { get; set; }
+        /// <summary>
+        /// A data item to identify the thing being dragged and for rendering the DragContent
+        /// </summary>
+        [Parameter] protected TItem DataItem { get; set; }
+        /// <summary>
+        /// HTML5 drag type allowed for this item - required - default is "move"
+        /// </summary>
+        [Parameter] protected string DragType { get; set; } = "move";
+        /// <summary>
+        /// CSS class for the draggable item - optional
+        /// </summary>
+        [Parameter] protected string DraggableClass { get; set; }
+        /// <summary>
+        /// Should you need to apply an inline style, I'm not going to stop you
+        /// </summary>
+        [Parameter] protected string DraggableStyle { get; set; }
+        /// <summary>
+        /// Flag that identifies if this item is the thing being dragged
+        /// </summary>
+        [Parameter] protected bool IsDragItem { get; set; }
+        /// <summary>
+        /// CSS class to apply when IsDragItem is true
+        /// </summary>
+        [Parameter] protected string DragItemClass { get; set; }
+        /// <summary>
+        /// Flag to enable console logging
+        /// </summary>
+        [Parameter] protected bool Debug { get; set; }
+
+        [Parameter] protected Action<UIDragEventArgs, TItem> OnDragStart { get; set; }
+        [Parameter] protected Action<UIDragEventArgs, TItem> OnDragEnd { get; set; }
+
+        string ClassList => new CssBuilder()
+          .AddClass(DraggableClass, !IsDragItem)
+          .AddClass(DragItemClass, IsDragItem)
+          .Build();
+
+        void MyDragStart(UIDragEventArgs args)
+        {
+            if (Debug) Console.WriteLine($"DS: {DataItem}");
+            args.DataTransfer.EffectAllowed = DragType;
+            args.DataTransfer.Types = new string[] { "text/plain" };
+            args.DataTransfer.Items = new UIDataTransferItem[] { new UIDataTransferItem() { Kind = "string", Type = "text/plain" } };
+            OnDragStart?.Invoke(args, DataItem);
+        }
+        void MyDragEnd(UIDragEventArgs args)
+        {
+            if (Debug) Console.WriteLine($"DE: {DataItem}");
+            OnDragEnd?.Invoke(args, DataItem);
+        }
+        string DragStartJS => "event.dataTransfer.effectAllowed = 'move'; event.dataTransfer.setData('text/plain', event.target.id);";
+        string DragEndJS => "event.stopPropagation();";
+
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            int c = 0;
+            builder.OpenElement(c++, "drag-item");
+            builder.AddAttribute(c++, "id", ID);
+            builder.AddAttribute(c++, "class", ClassList);
+            builder.AddAttribute(c++, "draggable", "true");
+            builder.AddAttribute(c++, "style", DraggableStyle);
+            builder.AddAttribute(c++, "ondragstart", DragStartJS);
+            builder.AddAttribute(c++, "ondragend", DragEndJS);
+            builder.AddAttribute(c++, "ondragstart", Microsoft.AspNetCore.Components.EventCallback.Factory.Create<Microsoft.AspNetCore.Components.UIDragEventArgs>(this, MyDragStart));
+            builder.AddAttribute(c++, "ondragend", Microsoft.AspNetCore.Components.EventCallback.Factory.Create<Microsoft.AspNetCore.Components.UIDragEventArgs>(this, MyDragEnd));
+            builder.AddContent(c++, DragContent(DataItem));
+            builder.CloseElement();
+        }
+    }
+}
---
+++ Blello/Components/DragAndDrop/DropZone.cs
@@ -0,0 +1,101 @@
+﻿using Blello.Helpers;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blello.Components.DragAndDrop
+{
+    public class DropZone<TItem> : ComponentBase
+    {
+        //@typeparam TItem
+        //<drop-outer class=@OuterClassList
+        //            ondragleave=@MyDragLeave
+        //            ondrop=@MyDragDrop
+        //            ondragenter=@MyDragEnter
+        //            ondragover="@DragOverJS"
+        //            ondragleave="@DragLeaveJS"
+        //            ondrop="@DragDropJS">
+        //        @DropContent(DataItem)
+        //</drop-outer>
+        //@functions
+        /// <summary>
+        /// Place your own markup / razor syntax in here
+        /// </summary>
+        [Parameter] protected RenderFragment<TItem> DropContent { get; set; }
+        /// <summary>
+        /// Data that will identify this drop zone
+        ///</summary>
+        [Parameter] protected TItem DataItem { get; set; }
+        /// <summary>
+        /// What type of drop to allow - default to "move"
+        ///</summary>
+        [Parameter] protected string DropType { get; set; } = "move";
+        /// <summary>
+        /// CSS class to use on the dropzone - default is "border border-normal"
+        ///</summary>
+        [Parameter] protected string DropZoneClass { get; set; } = "border border-normal";
+        /// <summary>
+        /// CSS class to use when this is the active dropzone
+        ///</summary>
+        [Parameter] protected string ActiveClass { get; set; }
+        /// <summary>
+        /// Flag to indicate if this is the active dropzone
+        ///</summary>
+        [Parameter] protected bool IsDropTarget { get; set; }
+        /// <summary>
+        /// Flag to enable console logging
+        ///</summary>
+        [Parameter] protected bool Debug { get; set; }
+
+        [Parameter] protected Action<UIDragEventArgs, object> OnDragEnter { get; set; }
+        [Parameter] protected Action<UIDragEventArgs, object> OnDragDrop { get; set; }
+        [Parameter] protected Action<UIDragEventArgs, object> OnDragLeave { get; set; }
+
+        string OuterClassList => new CssBuilder()
+            .AddClass(DropZoneClass)
+            .AddClass(ActiveClass, IsDropTarget)
+            .Build();
+
+        string DragOverJS => $"if (event.preventDefault) {{ event.preventDefault(); }}; event.dataTransfer.dropEffect = '{DropType}'; return false;";
+        string DragDropJS => ""; //$"if (event.stopPropagation) {{ event.stopPropagation(); }}; return false;";
+        string DragLeaveJS => "";// $"if (event.stopPropagation) {{ event.stopPropagation(); }}; return false;";
+
+        void MyDragEnter(UIDragEventArgs args)
+        {
+            if (Debug) Console.WriteLine($"DZ:{DataItem} ENTER");
+            IsDropTarget = true;
+            OnDragEnter?.Invoke(args, DataItem);
+        }
+
+        void MyDragLeave(UIDragEventArgs args)
+        {
+            if (Debug) Console.WriteLine($"DZ:{DataItem} LEAVE");
+            IsDropTarget = false;
+            OnDragLeave?.Invoke(args, DataItem);
+        }
+
+        void MyDragDrop(UIDragEventArgs args)
+        {
+            if (Debug) Console.WriteLine($"DZ:{DataItem} DROP");
+            IsDropTarget = false;
+            OnDragDrop?.Invoke(args, DataItem);
+        }
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.RenderTree.RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            int c = 0;
+            builder.OpenElement(c++, "drop-outer");
+            builder.AddAttribute(c++, "class", OuterClassList);
+            builder.AddAttribute(c++, "ondragleave", DragLeaveJS);
+            builder.AddAttribute(c++, "ondrop", DragDropJS);
+            builder.AddAttribute(c++, "ondragover", DragOverJS);
+            builder.AddAttribute(c++, "ondragleave", Microsoft.AspNetCore.Components.EventCallback.Factory.Create<Microsoft.AspNetCore.Components.UIDragEventArgs>(this, MyDragLeave));
+            builder.AddAttribute(c++, "ondrop", Microsoft.AspNetCore.Components.EventCallback.Factory.Create<Microsoft.AspNetCore.Components.UIDragEventArgs>(this, MyDragDrop));
+            builder.AddAttribute(c++, "ondragenter", Microsoft.AspNetCore.Components.EventCallback.Factory.Create<Microsoft.AspNetCore.Components.UIDragEventArgs>(this, MyDragEnter));
+            builder.AddContent(c++, DropContent(DataItem));
+            builder.CloseElement();
+        }
+    }
+}
